67a83325a7366570e309b4dbe6c971f8df704379
Alejandro Robert <75962547+aramaro19@users.noreply.github.com>
2024-05-23 15:16:51 -0400

    part 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
       <Button text="good" handleClick={handleGood} />
       <Button text="neutral" handleClick={handleNeutral} />
       <Button text="bad" handleClick={handleBad} />
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />  
     </div>
   )
@@ -24,29 +25,28 @@ const App = () => {
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
 const Statistics = ({ good, neutral, bad }) => {
+  const totalFeedback = good + neutral + bad
   const averageFeedback = (good - bad) / (good + neutral + bad)
   const positivePercentFeedback = good / (good + neutral + bad)
 
   if(good === 0 && neutral === 0 && bad === 0) {
     return (
       <div>
-         <h1>statistics</h1>
-         <p>No feedback given</p>
+         No feedback given
       </div>
     )
   } else return (
     <div>
-      <h1>statistics</h1>
-      <p>
-        good {good}<br></br>
-        neutral {neutral} <br></br>
-        bad {bad}<br></br>
-        all {good + neutral + bad} <br></br>
-        average {averageFeedback} <br></br> 
-        positive {positivePercentFeedback}
-      </p>   
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={totalFeedback} />
+        <StatisticLine text="average" value={averageFeedback} />
+        <StatisticLine text="positive" value={positivePercentFeedback} />
     </div>
   )
 }
+
+const StatisticLine = ({ text, value }) => <div>{text} {value}</div>
 
 export default App
